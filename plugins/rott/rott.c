c4f88f8a34505fd761a1fe434f8d609b0ff37580
erysdren <contact@erysdren.me>
2025-01-23 21:18:46 -0600

    Add plugin for Rise of the Triad formats support

---
+++ plugins/rott/rott.c
@@ -0,0 +1,13 @@
+#include "../plugin.h"
+
+qboolean FSROTT_Init(void);
+
+qboolean Plug_Init(void)
+{
+	char plugname[] = "rott";
+	plugfuncs->GetPluginName(0, plugname, sizeof(plugname));
+
+	if (!FSROTT_Init()) return false;
+
+	return true;
+}
