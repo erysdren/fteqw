1fcf9bf02903662ef0014d15755330449986cad4
erysdren <contact@erysdren.me>
2025-07-17 00:16:56 -0500

    add voxlap plugin for various voxel model formats

---
+++ plugins/voxlap/voxlap.h
@@ -0,0 +1,13 @@
+/*
+ * written by erysdren (it/its) in 2025
+ */
+#ifndef _VOXLAP_H_
+#define _VOXLAP_H_
+
+#include "../plugin.h"
+#include "../engine/common/com_mesh.h"
+
+extern plugmodfuncs_t *modfuncs;
+extern cvar_t *mod_voxlap_scale;
+
+#endif // _VOXLAP_H_
