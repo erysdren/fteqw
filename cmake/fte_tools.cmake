1293b29e856e55bac73c22436e950e04f9fc8f02
erysdren <contact@erysdren.me>
2026-01-11 11:36:51 -0600

    CMakeLists.txt: add fteqcc

--- cmake/fte_tools.cmake
+++ cmake/fte_tools.cmake
@@ -70,3 +70,22 @@ if(FTE_TOOL_ASM)
 	)
 	target_include_directories(q3asm2 PRIVATE ${FTE_ENGINE_QCLIB_DIR})
 endif()
+
+# fteqcc
+if(FTE_TOOL_QCC)
+	fte_add_tool(fteqcc
+		SOURCES
+			${FTE_ENGINE_QCLIB_DIR}/qcctui.c
+			${FTE_ENGINE_QCLIB_DIR}/packager.c
+			${FTE_ENGINE_QCLIB_DIR}/decomp.c
+			${FTE_ENGINE_QCLIB_DIR}/comprout.c
+			${FTE_ENGINE_QCLIB_DIR}/hash.c
+			${FTE_ENGINE_QCLIB_DIR}/qcc_cmdlib.c
+			${FTE_ENGINE_QCLIB_DIR}/qccmain.c
+			${FTE_ENGINE_QCLIB_DIR}/qcc_pr_comp.c
+			${FTE_ENGINE_QCLIB_DIR}/qcc_pr_lex.c
+			${FTE_ENGINE_QCLIB_DIR}/qcd_main.c
+	)
+	target_include_directories(fteqcc PRIVATE ${FTE_ENGINE_QCLIB_DIR})
+	target_link_libraries(fteqcc PRIVATE ZLIB::ZLIB $<TARGET_NAME_IF_EXISTS:Math::Math>)
+endif()
