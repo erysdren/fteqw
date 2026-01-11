588282bf932580ade29f4e755f7fc6afb818fc14
erysdren <contact@erysdren.me>
2026-01-11 09:47:51 -0600

    CMakeLists.txt: update

---
+++ cmake/fte_revision.cmake
@@ -0,0 +1,47 @@
+
+if(EXISTS ${CMAKE_SOURCE_DIR}/.git AND NOT DEFINED FTE_REVISON)
+	EXECUTE_PROCESS(COMMAND
+		git describe --always --long --dirty
+		WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+		OUTPUT_VARIABLE FTE_REVISON_GIT
+		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	EXECUTE_PROCESS(COMMAND
+		git log -1 --format=%cs
+		WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+		OUTPUT_VARIABLE FTE_DATE
+		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	EXECUTE_PROCESS(COMMAND
+		git branch --show-current
+		WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+		OUTPUT_VARIABLE FTE_BRANCH
+		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	EXECUTE_PROCESS(COMMAND
+		git rev-parse --is-shallow-repository
+		WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+		OUTPUT_VARIABLE FTE_GIT_IS_SHALLOW
+		ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	if(FTE_GIT_IS_SHALLOW STREQUAL true)
+		MESSAGE(STATUS "shallow clone prevents calculation of revision number.")
+		SET(SVNREVISION "git-${FTE_REVISON_GIT}")	#if its a shallow clone then we can't count commits properly so don't know what revision we actually are.
+	else()
+		EXECUTE_PROCESS(COMMAND
+			git rev-list HEAD --count
+			WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+			OUTPUT_VARIABLE SVNREVISION
+			ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		MATH(EXPR SVNREVISION "${SVNREVISION} + 29") #not all svn commits managed to appear im the git repo, so we have a small bias to keep things consistent.
+		if(FTE_BRANCH STREQUAL "master" OR FTE_BRANCH STREQUAL "")
+			SET(SVNREVISION "${SVNREVISION}-git-${FTE_REVISON_GIT}")
+		else()
+			SET(SVNREVISION "${FTE_BRANCH}-${SVNREVISION}-git-${FTE_REVISON_GIT}") #weird branches get a different form of revision, to reduce confusion.
+		endif()
+	endif()
+	MESSAGE(STATUS "FTE GIT ${FTE_BRANCH} Revision ${SVNREVISION}, ${FTE_DATE}")
+	SET(FTE_REVISON SVNREVISION=${SVNREVISION} SVNDATE=${FTE_DATE} FTE_BRANCH=${FTE_BRANCH})
+endif()
+
