4a6607066c5d7dcc4dca3d1a0d5fa017f91348a7
erysdren <contact@erysdren.me>
2026-01-23 13:21:23 -0600

    plugins/hl2.cmake: update

--- cmake/plugins/hl2.cmake
+++ cmake/plugins/hl2.cmake
@@ -15,5 +15,5 @@ fte_add_plugin(fteplug_hl2
 		${FTE_PLUGINS_ROOT_DIR}/hl2/hl2.c
 		${FTE_PLUGINS_ROOT_DIR}/plugin.c
 )
-target_include_directories(fteplug_hl2 PRIVATE ${FTE_PLUGINS_ROOT_DIR} ${FTE_PLUGINS_ROOT_DIR}/hl2 ${FTE_ENGINE_CLIENT_DIR} ${FTE_ENGINE_QCLIB_DIR} ${FTE_ENGINE_GL_DIR})
+target_include_directories(fteplug_hl2 PRIVATE ${FTE_PLUGINS_ROOT_DIR} ${FTE_PLUGINS_ROOT_DIR}/hl2 ${FTE_ENGINE_CLIENT_DIR} ${FTE_ENGINE_QCLIB_DIR} ${FTE_ENGINE_GL_DIR} ${FTE_ENGINE_COMMON_DIR})
 target_compile_definitions(fteplug_hl2 PRIVATE MULTITHREAD)
