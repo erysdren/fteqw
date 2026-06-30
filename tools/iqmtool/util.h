a469b797f8eb1966fb53f8501ab1d0268334ae29
erysdren <contact@erysdren.me>
2026-06-30 16:05:00 -0500

    iqmtool: remove useless crap in util.h

--- tools/iqmtool/util.h
+++ tools/iqmtool/util.h
@@ -34,28 +34,6 @@ typedef unsigned int uint;
 typedef signed long long int llong;
 typedef unsigned long long int ullong;
 
-/*inline void *operator new(size_t size)
-{
-	void *p = malloc(size);
-	if(!p) abort();
-	return p;
-}
-inline void *operator new[](size_t size)
-{
-	void *p = malloc(size);
-	if(!p) abort();
-	return p;
-}
-inline void operator delete(void *p) { if(p) free(p); }
-inline void operator delete[](void *p) { if(p) free(p); }
-inline void operator delete(void *p, size_t sz) { if(p) free(p); }
-inline void operator delete[](void *p, size_t sz) { if(p) free(p); }
-*/
-inline void *operator new(size_t, void *p) { return p; }
-inline void *operator new[](size_t, void *p) { return p; }
-inline void operator delete(void *, void *) {}
-inline void operator delete[](void *, void *) {}
-
 #ifdef swap
 #undef swap
 #endif
