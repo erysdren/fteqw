e602c2f21875acb02afd8c701bda83a31456ea10
erysdren <contact@erysdren.me>
2026-07-04 11:24:51 -0500

    plugins: winamp

---
+++ cmake/plugins/winamp.cmake
@@ -0,0 +1,10 @@
+if(NOT FTE_PLUGIN_WINAMP)
+	return()
+endif()
+
+fte_add_plugin(fteplug_winamp
+	SOURCES
+		${FTE_PLUGINS_ROOT_DIR}/winamp/winamp.c
+		${FTE_PLUGINS_ROOT_DIR}/plugin.c
+)
+target_include_directories(fteplug_winamp PRIVATE ${FTE_PLUGINS_ROOT_DIR} ${FTE_PLUGINS_ROOT_DIR}/winamp)
