d7ce8ff417b4fdcd65fa06bf3abbd20fc9fd4bb8
erysdren <contact@erysdren.me>
2026-07-04 17:53:27 -0500

    plugins: fix ode on mingw

--- cmake/plugins/ode.cmake
+++ cmake/plugins/ode.cmake
@@ -5,6 +5,7 @@ endif()
 fte_add_plugin(fteplug_ode
 	SOURCES
 		${FTE_ENGINE_COMMON_DIR}/com_phys_ode.c
+		${FTE_ENGINE_COMMON_DIR}/mathlib.c
 		${FTE_PLUGINS_ROOT_DIR}/plugin.c
 )
 target_include_directories(fteplug_ode PRIVATE ${FTE_PLUGINS_ROOT_DIR} ${FTE_ENGINE_COMMON_DIR} ${FTE_ENGINE_CLIENT_DIR} ${FTE_ENGINE_QCLIB_DIR} ${FTE_ENGINE_GL_DIR})
