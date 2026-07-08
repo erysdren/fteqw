f0b43836dc09c2c4e4341114470ec2f55533181b
erysdren <contact@erysdren.me>
2026-07-08 16:00:04 -0500

    bothdefs.h: fix warning on msvc

--- engine/common/bothdefs.h
+++ engine/common/bothdefs.h
@@ -845,8 +845,12 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #ifdef _WIN32
+#ifndef ZEXPORT
 #define ZEXPORT VARGS
+#endif
+#ifndef ZEXPORTVA
 #define ZEXPORTVA VARGS
+#endif
 #endif
 
 #ifdef _DEBUG
